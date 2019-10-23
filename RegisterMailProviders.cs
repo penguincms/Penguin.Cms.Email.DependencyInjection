a2a9d78138e5b0700f3409a98ef7fbba4c4d5055
anloffredo <loffredo.ap@gmail.com>
2019-10-23 00:54:53 -0700

    Framework extraction update
    Framework extraction update

---
+++ RegisterMailProviders.cs
@@ -0,0 +1,24 @@
+﻿using Penguin.Cms.Email.Services;
+using Penguin.Cms.Email.Templating.Repositories;
+using Penguin.DependencyInjection.Abstractions.Enums;
+using Penguin.DependencyInjection.Abstractions.Interfaces;
+using Penguin.DependencyInjection.ServiceProviders;
+using Penguin.Email.Abstractions.Interfaces;
+using Penguin.Email.Templating;
+using Penguin.Email.Templating.Abstractions.Interfaces;
+using System;
+
+namespace Penguin.Cms.Email.DependencyInjection
+{
+    public class RegisterMailProviders : IRegisterDependencies
+    {
+        public void RegisterDependencies(IServiceRegister serviceRegister)
+        {
+            serviceRegister.Register(typeof(ISendTemplates), typeof(EmailTemplateRepository), ServiceLifetime.Scoped);
+            serviceRegister.Register(typeof(IQueueAndSendMail), typeof(EmailService), ServiceLifetime.Scoped);
+            serviceRegister.Register(typeof(IQueueAndSendMail), typeof(EmailService), ServiceLifetime.Scoped);
+            serviceRegister.Register(typeof(ISendMail), typeof(EmailService), ServiceLifetime.Scoped);
+            serviceRegister.Register(typeof(EmailRenderer), typeof(EmailRenderer), ServiceLifetime.Scoped);
+        }
+    }
+}
